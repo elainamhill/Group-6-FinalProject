31e1d5ce1f95b9b0206e2018ac3feddcb7fb2476
Elaina Hill <elainamhill@gmail.com>
2022-11-06 10:39:36 -0500

    Modify validFormFieldInput function to close alert

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,51 +3,64 @@
 
 const validFormFieldInput = () => {
 
+    // Input element variables
     const taskNameInput = document.querySelector('#input_taskName');
     const taskDescriptionInput = document.querySelector('#input_description');
     const taskAssignInput = document.querySelector('#input_assign');
     const taskDueDateInput = document.querySelector('#input_dueDate');
 
+    // Input with value variables
     const name = taskNameInput.value;
     const description = taskDescriptionInput.value;
     const assign = taskAssignInput.value;
     const due = taskDueDateInput.value;
 
     // Test New Task form input values
+    // console.log(typeof name);
     console.log("Task Name: " + name);
-    console.log(typeof name);
     console.log("Description: " + description);
     console.log("Assigned To: " + assign);
     console.log("Due Date: " + due);
 
-    // Form alert appears when field is:
-    // - left blank/empty
-    let formAlert = document.getElementById("form_alert");
+    // Form alert appears when field is blank/empty
+    const formAlert = document.getElementById("form_alert");
+    const form = document.querySelector(".require-valid");
 
+    form.checkValidity();
+    form.classList.add('was-validated');
+
+    if(!form.checkValidity()) {
+        sendAlert(formAlert);
+        return false;
+    }
+
+    /* 
     if (name === "") {
         formAlert.style.display = "block";
-        formAlert.innerHTML = "Submission error: task name required! Please correct and submit to continue.";
         return false;
     }
 
     if (description === "") {
         formAlert.style.display = "block";
-        formAlert.innerHTML = "Submission error: task description required! Please correct and submit to continue.";
         return false;
     }
 
     if (assign === "") {
         formAlert.style.display = "block";
-        formAlert.innerHTML = "Submission error: task assignment required! Please correct and submit to continue.";
         return false;
     }
 
     if (due === "") {
         formAlert.style.display = "block";
-        formAlert.innerHTML = "Submission error: task due date required! Please correct and submit to continue.";
         return false;
-    }
+    } */
 
     formAlert.style.display = "none";
     return true;
+}
+
+// Show alert, close after 5 seconds
+function sendAlert(alert) {
+    alert.style.display = "block";
+    setTimeout(() => {alert.style.display = "none"}, 5000);
 }
